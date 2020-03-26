2572fef62e23c2546492d3bdd474c9f4587a02eb
Deigan Millikin <deiganmillikin@gmail.com>
2020-03-25 20:14:46 -0600

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About Gatsby" />
+    <Header headerText="It's pretty cool" />
+    <Header headerText="Bee boop" />
+
+    <p>Such wow. Very React.</p>
+  </div>
+)
